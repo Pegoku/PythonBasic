1241c2e1f63c3829bf45de7f712b99342781f709
Pegoku <pegoku08@gmail.com>
2024-11-22 13:21:29 +0100

    update

--- t10ex12.py
+++ t10ex12.py
@@ -26,27 +26,36 @@ def elevar():
     print("El resultat de l'operacio es: ", num ** exponent)
 
 def canviar_base():
-    option = int(input("Vols convertir de (1: decimal a binari, 2: binari a decimal:, 3: decimal a hexadecimal, 4: hexadecimal a decimal:, 5: decimal a octal, 6: octal a decimal): "))
+    option = int(input("Vols convertir de (1: binari, 2: octal:, 3: decimal, 4: hexadecimal): "))
     match(option):
         case 1:
             num = int(input("Introdueix el numero: "))
-            print("El numero en binari es: ", bin(num))
+            print("El numero en binari es: ", bin(int(str(num), 2)))
+            print("El numero en octal es: ", oct(int(str(num), 2)))
+            print("El numero en decimal es: ", int(str(num), 2))
+            print("El numero en hexadecimal es: ", hex(int(str(num), 2)))
+            
         case 2:
             num = input("Introdueix el numero: ")
-            print("El numero en decimal es: ", int(str(num), 2))
+            print("El numero en binari es: ", bin(int(str(num), 8)))
+            print("El numero en octal es: ", oct(int(str(num), 8)))
+            print("El numero en decimal es: ", int(str(num), 8))
+            print("El numero en hexadecimal es: ", hex(int(str(num), 8)))
+            
         case 3:
             num = int(input("Introdueix el numero: "))
-            print("El numero en hexadecimal es: ", hex(num))
+            print("El numero en binari es: ", bin(int(str(num), 10)))
+            print("El numero en octal es: ", oct(int(str(num), 10)))
+            print("El numero en decimal es: ", int(str(num), 10))
+            print("El numero en hexadecimal es: ", hex(int(str(num), 10)))
+        
         case 4:
             num = input("Introdueix el numero: ")
+            print("El numero en binari es: ", bin(int(str(num), 16)))
+            print("El numero en octal es: ", oct(int(str(num), 16)))
             print("El numero en decimal es: ", int(str(num), 16))
-        case 5: 
-            num = int(input("Introdueix el numero: "))
-            print("El numero en octal es: ", oct(num))
-        case 6:
-            num = input("Introdueix el numero: ")
-            print("El numero en decimal es: ", int(str(num), 8))
-    
+            print("El numero en hexadecimal es: ", hex(int(str(num), 16)))
+        
 def calc():
     option = int(input("Quina operacio vols fer? (1: suma, 2: resta, 3: multiplicacio, 4: divisio, 5: elevar, 6: canviar base:, 7: sortir): "))
     match(option):
